e88f6c24ea7e8b8956647f472b65ae99ca6a3e18
Vinoth Kannan <svkn.87@gmail.com>
2025-12-04 14:06:45 +0530

    dev: Implement state management for CRUD operations in CrudExample component, enabling dynamic data updates for create, edit, and delete actions.

--- components/crud-example.tsx
+++ components/crud-example.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Crud, CrudForm } from "@/registry/new-york/blocks/crud"
+import { useState } from "react"
 
 type Payment = {
   id: string
@@ -22,12 +23,12 @@ export const payments: Payment[] = [
     status: "processing",
     email: "example@gmail.com",
   },
-  // ...
 ]
 
 export default function CrudExample() {
+  const [data, setData] = useState(payments)
   return (
-    <Crud name="Payment" data={payments} columns={[
+    <Crud name="Payment" data={data} columns={[
       {
         accessorKey: "id",
         header: "ID",
@@ -44,7 +45,18 @@ export default function CrudExample() {
         accessorKey: "email",
         header: "Email",
       },
-    ]} onEdit={(row) => console.log(row)} onDelete={(row) => console.log(row)} onCreate={(e) => console.log(e)}>
+    ]} onEdit={(row) => {
+      setData(data.map((item) => item.id === row.id ? row : item))
+    }} onDelete={(row) => {
+      setData(data.filter((item) => item.id !== row.id))
+    }} onCreate={(e) => {
+      setData([...data, {
+        id: crypto.randomUUID(),
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      }])
+    }}>
       <CrudForm>
         <div>Form Content</div>
       </CrudForm>
